c78ac1234423852fbe60ab192d94abfbda5bebb7
mohdshebinnnc <mohammedshebinc92@gmail.com>
2025-09-08 17:51:36 +0530

    Done

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Welcome to My App</Button>
+    <div className="mt-40">
+      <Button variant="destructive">Welcome to My App</Button>
     </div>
   );
 }
--- components/header.jsx
+++ components/header.jsx
@@ -2,32 +2,62 @@ import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@cl
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
+import { Button } from './ui/button';
+import { LayoutDashboard, PenBox } from 'lucide-react';
 
 const Header = () => {
-  return (
-    <div className='fixed top-0'>
-        <nav>
-            <Link href="/">
-                <Image src="/logo.png" alt="Logo" width={200} height={60} 
-                    className='cursor-pointer h-12 w-auto object-contain'
-                />
-            </Link>
-        </nav>
+    return (
+        <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 px-4 py-2 shadow-md border-b'>
+            <nav className='container mx-auto flex px-4 py-3 items-center justify-between'>
+                <Link href="/">
+                    <Image src="/logo.png" alt="Logo" width={200} height={60} 
+                        className='cursor-pointer h-12 w-auto object-contain'
+                    />
+                </Link>
 
+                <div className='flex items-center space-x-4'>
+                    <SignedIn>
+                        <Link href="/dashboard" 
+                            className='text-gray-600 hover:text-blue-600 flex items-center gap-2'
+                        >
+                            <Button variant="outline">
+                                <LayoutDashboard size={18}/>
+                                <span className='hidden md:inline'>Dashboard</span>
+                            </Button>
+                        </Link>
 
-        <SignedOut>
-            <SignInButton/>
-            <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                    Sign Up
-                </button>
-            </SignUpButton>
-        </SignedOut>
-        <SignedIn>
-            <UserButton />
-        </SignedIn>
-    </div>
-  )
+                        <Link href="/transaction/create">
+                            <Button className="flex items-center gap-2">
+                                <PenBox size={18}/>
+                                <span className='hidden md:inline'>Add Transaction</span>
+                            </Button>
+                        </Link>
+                    </SignedIn>
+
+                    <SignedOut>
+                        <SignInButton forceRedirectUrl='/dashboard'>
+                            <Button variant="outline">Login</Button>
+                        </SignInButton>
+                        {/* <SignUpButton/> */}
+                            {/* <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                Sign Up
+                            </button> */}
+                        {/* </SignUpButton> */}
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton 
+                            appearance={{
+                                elements: {
+                                    avatarBox: 'w-10 h-10',
+                                    
+                                }
+                            }}
+                        />
+                    </SignedIn>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Header;
